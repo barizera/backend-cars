b10bbf84c8c9b26fe1851f2e58bdd3f1640a1aee
Felipe Bari <luizfelipebari@gmail.com>
2022-06-13 22:42:45 -0300

    dot env

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const { connect } = mongoose;
 
 export const connectToDatabase = () => {
-  connect(
-    process.env.DATABASE_URL,
+  connect(process.env.DATABASE_URL,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
